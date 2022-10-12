e144efb4f24ce2d0b83f58afb6d9568d1774fa77
zorro27 <73430357+zorro27@users.noreply.github.com>
2022-10-12 20:53:37 +0300

    Initial Commit

---
+++ FinCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinCalculator
+//
+//  Created by Роман Зобнин on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
