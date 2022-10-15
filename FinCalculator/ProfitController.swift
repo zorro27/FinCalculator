10946551e9d5b3ef6a439921bc2bcb4c7590c42e
zorro27 <73430357+zorro27@users.noreply.github.com>
2022-10-15 19:53:30 +0300

    Добавлена логика для первого экрана

--- FinCalculator/ProfitController.swift
+++ FinCalculator/ProfitController.swift
@@ -11,27 +11,54 @@ class ProfitController: UIViewController {
     
     @IBOutlet weak var BalanceLabel: UILabel!
     @IBOutlet weak var ValueLabel: UILabel!
-    @IBOutlet weak var AddProfitButton: UIButton!
     @IBOutlet weak var ProfitTable: UITableView!
+    @IBOutlet weak var AddProfitButton: UIButton!
     
-    let a = ["1","2","3"]
+//    testTable.append("1")
+//    totalScore += 500
+//    ValueLabel.text = String(totalScore) + "руб."
+//    print (totalScore)
+//    ProfitTable.reloadData()
+    
+    var testTable: [String] = []
+    var totalScore: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ProfitTable.dataSource = self
         self.ProfitTable.delegate = self
+        settingsButton(button: AddProfitButton)
+        pushButton()
+        ValueLabel.text = "0 руб."
     }
 }
 
 extension ProfitController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        a.count
+        testTable.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.ProfitTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = a[indexPath.row]
-        print("таблица дошла")
+        cell.textLabel?.text = testTable[indexPath.row]
         return cell
+    }
+    
+    func settingsButton(button: UIButton) {
+        button.titleLabel?.text = "Добавить доход"
+        button.backgroundColor = .blue
+        button.alpha = 0.6
+        button.tintColor = .white
+        button.layer.cornerRadius = 10
+    }
+    
+    func pushButton() {
+        AddProfitButton.addTarget(self, action: #selector(addProfit), for: .touchUpInside)
+    }
+    @objc func addProfit() {
+        testTable.append("1")
+        totalScore += 500
+        ValueLabel.text = String(totalScore) + "руб."
+        ProfitTable.reloadData()
     }
 }
